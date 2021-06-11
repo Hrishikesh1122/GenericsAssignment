87f05bc4e9437747b82238090f8c097c08bb959d
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-11 15:05:57 +0530

    Added : Maximum of Doubles

--- Maximum.java
+++ Maximum.java
@@ -22,10 +22,25 @@ public class Maximum {
 			max=num3;
 		return max;	
 	}
+	/**
+	 * @param num1
+	 * @param num2
+	 * @param num3
+	 * @return Maximum of 3 Doubles
+	 */
+	public  Double maxDouble(Double num1 ,Double num2 , Double num3) {
+		Double max=num1;
+		if(num2.compareTo(max)>0)
+			max=num2;
+		if(num3.compareTo(max)>0)
+			max=num3;
+		return max;	
+	}
 
 	public static void main(String[] args) {
 		Maximum m = new Maximum();
 		System.out.println(m.maxInt(-3,55,8));
+		System.out.println(m.maxDouble(-3.5,10.5,-85.5));
 
 	}
 
